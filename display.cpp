39a08db1c6fceb8b93317362782c452696c6cade
Rune Olsen <rbo@pirat.it>
2012-07-27 11:55:07 +0800

    Added CapSence library. Added uppercase alphabet. Minor fixes.

--- display.cpp
+++ display.cpp
@@ -26,70 +26,193 @@ int getSelectedInput()
 
 void inputSelect(int n)
 {
+   Serial.print("Input select: ");
+   Serial.println(n);
    switch(n)
    {
       case 0:
-        writeChar('A', 0);
-        writeChar(' ', 1);
+        writeChar('G', 0);
+        writeChar('O', 1);
         writeChar('D', 2);
-        writeChar('V', 3);
-        writeChar('D', 4);
+        writeChar(' ', 3);
+        writeChar(' ', 4);
         writeChar(' ', 5);
         writeChar(' ', 6);
         writeChar(' ', 7);
         break; 
 
       case 1:
-        writeChar('A', 0);
-        writeChar(' ', 1);
-        writeChar(' ', 2);
-        writeChar('C', 3);
-        writeChar('D', 4);
-        writeChar(' ', 5);
-        writeChar(' ', 6);
+        writeChar('B', 0);
+        writeChar('E', 1);
+        writeChar('D', 2);
+        writeChar('R', 3);
+        writeChar('I', 4);
+        writeChar('N', 5);
+        writeChar('G', 6);
         writeChar(' ', 7);
         break; 
 
       case 2:
-        writeChar('A', 0);
-        writeChar(' ', 1);
-        writeChar(' ', 2);
-        writeChar('T', 3);
-        writeChar('V', 4);
+        writeChar('M', 0);
+        writeChar('E', 1);
+        writeChar('D', 2);
+        writeChar(' ', 3);
+        writeChar(' ', 4);
         writeChar(' ', 5);
         writeChar(' ', 6);
         writeChar(' ', 7);
         break; 
+
+      case 3:
+        writeChar('F', 0);
+        writeChar('O', 1);
+        writeChar('D', 2);
+        writeChar('E', 3);
+        writeChar('N', 4);
+        writeChar(' ', 5);
+        writeChar(' ', 6);
+        writeChar(' ', 7);
+        break; 
+
+      case 4:
+        writeChar('H', 0);
+        writeChar('E', 1);
+        writeChar('L', 2);
+        writeChar('G', 3);
+        writeChar('A', 4);
+        writeChar(' ', 5);
+        writeChar(' ', 6);
+        writeChar(' ', 7);
+        break; 
+
+      case 5:
+        writeChar(' ', 0);
+        writeChar(' ', 1);
+        writeChar(' ', 2);
+        writeChar(' ', 3);
+        writeChar(' ', 4);
+        writeChar(' ', 5);
+        writeChar(' ', 6);
+        writeChar(' ', 7);
+        break; 
+
+      case 6:
+        writeChar('G', 0);
+        writeChar('G', 1);
+        writeChar('G', 2);
+        writeChar('G', 3);
+        writeChar('G', 4);
+        writeChar('G', 5);
+        writeChar('G', 6);
+        writeChar('G', 7);
+        break; 
+
+      case 7:
+        writeChar('H', 0);
+        writeChar('H', 1);
+        writeChar('H', 2);
+        writeChar('H', 3);
+        writeChar('H', 4);
+        writeChar('H', 5);
+        writeChar('H', 6);
+        writeChar('H', 7);
+        break; 
+
+      default:
+        writeChar('N', 0);
+        writeChar('O', 1);
+        writeChar('T', 2);
+        writeChar(' ', 3);
+        writeChar('D', 4);
+        writeChar('O', 5);
+        writeChar('N', 6);
+        writeChar('E', 7);
+        break; 
    }
 }
 
+void write_please()
+{
+  writeChar(' ', 0);
+  writeChar('P', 1);
+  writeChar('L', 2);
+  writeChar('E', 3);
+  writeChar('A', 4);
+  writeChar('S', 5);
+  writeChar('E', 6);
+  writeChar(' ', 7);
+}
+
+void write_wait()
+{
+  writeChar(' ', 0);
+  writeChar(' ', 1);
+  writeChar('W', 2);
+  writeChar('A', 3);
+  writeChar('I', 4);
+  writeChar('T', 5);
+  writeChar(' ', 6);
+  writeChar(' ', 7);
+}
+
+void displayWarmup()
+{
+   Serial.println("Inside warmup loop.");
+   setBrightness(0);
+   for(int i=0;i<2;i++)
+   {
+      write_please();
+      for(int j=0;j<10;j++)
+      {
+	 setBrightness(0);
+	 delay(100);
+	 setBrightness(6);
+	 delay(100);
+      }
+      write_wait();
+      for(int j=0;j<10;j++)
+      {
+	 setBrightness(0);
+	 delay(100);
+	 setBrightness(6);
+	 delay(100);
+      }
+   }
+   Serial.println("Inside warmup loop. DONE.");
+   digitalWrite(3, HIGH);
+}
 
 void setup()
 {                
+  delay(100);
+  Serial.begin(9600);		// for debugging
+  Serial.println("Starting.");
   initSPI();
   pinMode(2, INPUT);     
-  pinMode(3, INPUT);     
+  pinMode(3, OUTPUT);     
+  digitalWrite(3, LOW);
+  displayWarmup();
+  inputSelect(getSelectedInput());
 }
-
 
 void loop()
 {
-  int selectedInput = getSelectedInput();
-  setBrightness(0);
-  inputSelect(selectedInput);
-  if(selectedInput != currentInput)
-    {
+   int selectedInput = getSelectedInput();
+   setBrightness(0);
+   if(selectedInput != currentInput)
+   {
+      Serial.println("Input changed.");
       delay(50);
       selectedInput = getSelectedInput();
       currentInput = selectedInput;
       inputSelect(currentInput);
       lastChangeTime = millis();
       setBrightness(0);
-    }
+   }
     
-    if((lastChangeTime + 3000)<millis())
-       setBrightness(4);
-    if((lastChangeTime + 5000)<millis())
-       setBrightness(6);
+   if((lastChangeTime + 3000)<millis())
+      setBrightness(4);
+   if((lastChangeTime + 5000)<millis())
+      setBrightness(6);
 }
 
--- displayDriver.cpp
+++ displayDriver.cpp
@@ -7,7 +7,7 @@ void initSPI()
   SPI.begin();
   SPI.setBitOrder(LSBFIRST);
   SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV64);
+  SPI.setClockDivider(SPI_CLOCK_DIV32);
   delay(10);
 
   digitalWrite(9, LOW);
@@ -48,6 +48,27 @@ void write_blank()
   writeSpiChar(0x00);
 }
 
+void write_A()
+{
+  writeSpiChar(0x0E);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x1F);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+}
+
+void write_B()
+{
+  writeSpiChar(0x1E);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x1E);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x1E);
+}
 
 void write_C()
 {
@@ -60,7 +81,6 @@ void write_C()
   writeSpiChar(0x0E);
 }
 
-
 void write_D()
 {
   writeSpiChar(0x1E);
@@ -70,6 +90,171 @@ void write_D()
   writeSpiChar(0x11);
   writeSpiChar(0x11);
   writeSpiChar(0x1E);
+}
+
+void write_E()
+{
+  writeSpiChar(0x1F);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x1E);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x1F);
+}
+
+void write_F()
+{
+  writeSpiChar(0x1F);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x1E);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+}
+
+void write_G()
+{
+  writeSpiChar(0x0E);
+  writeSpiChar(0x11);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x13);
+  writeSpiChar(0x11);
+  writeSpiChar(0x0E);
+}
+
+void write_H()
+{
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x1F);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+}
+
+void write_I()
+{
+  writeSpiChar(0x0E);
+  writeSpiChar(0x04);
+  writeSpiChar(0x04);
+  writeSpiChar(0x04);
+  writeSpiChar(0x04);
+  writeSpiChar(0x04);
+  writeSpiChar(0x0E);
+}
+
+void write_J()
+{
+  writeSpiChar(0x01);
+  writeSpiChar(0x01);
+  writeSpiChar(0x01);
+  writeSpiChar(0x01);
+  writeSpiChar(0x01);
+  writeSpiChar(0x11);
+  writeSpiChar(0x0E);
+}
+
+void write_K()
+{
+  writeSpiChar(0x11);
+  writeSpiChar(0x12);
+  writeSpiChar(0x14);
+  writeSpiChar(0x18);
+  writeSpiChar(0x14);
+  writeSpiChar(0x12);
+  writeSpiChar(0x11);
+}
+
+void write_L()
+{
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x1F);
+}
+
+void write_M()
+{
+  writeSpiChar(0x11);
+  writeSpiChar(0x1B);
+  writeSpiChar(0x15);
+  writeSpiChar(0x15);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+}
+
+void write_N()
+{
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x19);
+  writeSpiChar(0x15);
+  writeSpiChar(0x13);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+}
+
+void write_O()
+{
+  writeSpiChar(0x0E);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x0E);
+}
+
+void write_P()
+{
+  writeSpiChar(0x1E);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x1E);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+  writeSpiChar(0x10);
+}
+
+void write_Q()
+{
+  writeSpiChar(0x0E);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x15);
+  writeSpiChar(0x12);
+  writeSpiChar(0x0D);
+}
+
+void write_R()
+{
+  writeSpiChar(0x1E);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x1E);
+  writeSpiChar(0x14);
+  writeSpiChar(0x12);
+  writeSpiChar(0x11);
+}
+
+void write_S()
+{
+  writeSpiChar(0x0E);
+  writeSpiChar(0x11);
+  writeSpiChar(0x10);
+  writeSpiChar(0x0E);
+  writeSpiChar(0x01);
+  writeSpiChar(0x11);
+  writeSpiChar(0x0E);
 }
 
 void write_T()
@@ -83,6 +268,17 @@ void write_T()
   writeSpiChar(0x04);
 }
 
+void write_U()
+{
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x0E);
+}
+
 void write_V()
 {
   writeSpiChar(0x11);
@@ -93,6 +289,62 @@ void write_V()
   writeSpiChar(0x04);
   writeSpiChar(0x04);
 }
+
+void write_W()
+{
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x15);
+  writeSpiChar(0x15);
+  writeSpiChar(0x1B);
+  writeSpiChar(0x11);
+}
+
+void write_X()
+{
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x0A);
+  writeSpiChar(0x04);
+  writeSpiChar(0x0A);
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+}
+
+void write_Y()
+{
+  writeSpiChar(0x11);
+  writeSpiChar(0x11);
+  writeSpiChar(0x0A);
+  writeSpiChar(0x04);
+  writeSpiChar(0x04);
+  writeSpiChar(0x04);
+  writeSpiChar(0x04);
+}
+
+void write_Z()
+{
+  writeSpiChar(0x1F);
+  writeSpiChar(0x01);
+  writeSpiChar(0x02);
+  writeSpiChar(0x04);
+  writeSpiChar(0x08);
+  writeSpiChar(0x10);
+  writeSpiChar(0x1F);
+}
+
+void write_colon()
+{
+  writeSpiChar(0x00);
+  writeSpiChar(0x0C);
+  writeSpiChar(0x0C);
+  writeSpiChar(0x00);
+  writeSpiChar(0x0C);
+  writeSpiChar(0x0C);
+  writeSpiChar(0x00);
+}
+
 
 void write_dash()
 {
@@ -105,6 +357,17 @@ void write_dash()
   writeSpiChar(0x00);
 }
 
+void write_checker()
+{
+  writeSpiChar(0x0A);
+  writeSpiChar(0x15);
+  writeSpiChar(0x0A);
+  writeSpiChar(0x15);
+  writeSpiChar(0x0A);
+  writeSpiChar(0x15);
+  writeSpiChar(0x0A);
+}
+
 
 void writeChar(char letter, int pos)
 {
@@ -115,6 +378,14 @@ void writeChar(char letter, int pos)
      write_blank();
      break;
      
+   case 'A':
+     write_A();
+     break;
+     
+   case 'B':
+     write_B();
+     break;
+     
    case 'C':
      write_C();
      break;
@@ -123,16 +394,100 @@ void writeChar(char letter, int pos)
      write_D();
      break;
      
+   case 'E':
+     write_E();
+     break;
+     
+   case 'F':
+     write_F();
+     break;
+     
+   case 'G':
+     write_G();
+     break;
+     
+   case 'H':
+     write_H();
+     break;
+     
+   case 'I':
+     write_I();
+     break;
+     
+   case 'J':
+     write_J();
+     break;
+     
+   case 'K':
+     write_K();
+     break;
+     
+   case 'L':
+     write_L();
+     break;
+     
+   case 'M':
+     write_M();
+     break;
+     
+   case 'N':
+     write_N();
+     break;
+     
+   case 'O':
+     write_O();
+     break;
+     
+   case 'P':
+     write_P();
+     break;
+     
+   case 'Q':
+     write_Q();
+     break;
+     
+   case 'R':
+     write_R();
+     break;
+     
+   case 'S':
+     write_S();
+     break;
+     
    case 'T':
      write_T();
      break;
 
+   case 'U':
+     write_U();
+     break;
+     
    case 'V':
      write_V();
      break;
 
+   case 'W':
+     write_W();
+     break;
+     
+   case 'X':
+     write_X();
+     break;
+     
+   case 'Y':
+     write_Y();
+     break;
+     
+   case 'Z':
+     write_Z();
+     break;
+     
+   case ':':
+     write_colon();
+     break;
+     
   default:
-    write_dash();
+    write_checker();
     break;
   } 
 }
